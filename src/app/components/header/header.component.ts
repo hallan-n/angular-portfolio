3a560ce540ce5affa84903f3a07b18bf88828781
hallan-n <hallanneves01@gmail.com>
2024-02-15 00:51:54 -0300

    feat: add more pages

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-// import * as SmoothScroll from 'smooth-scroll';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, MatIconModule, MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent { }
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, MatProgressBarModule],
   templateUrl: './about.component.html'
 })
 export class AboutComponent {
