29d63961e7e18c5c369bb84b979a94762fe791c9
hallan-n <hallanneves01@gmail.com>
2024-02-15 18:44:12 -0300

    feat: create about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,12 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NgOptimizedImage, MatProgressBarModule],
+  imports: [NgOptimizedImage, MatProgressBarModule, MatButtonModule],
   templateUrl: './about.component.html'
 })
-export class AboutComponent {
-
-}
+export class AboutComponent { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgOptimizedImage],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent { }
