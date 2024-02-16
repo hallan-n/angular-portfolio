5f4866d91dc674b9e3281c82242bf1c5f52c7b75
hallan-n <hallanneves01@gmail.com>
2024-02-15 21:05:26 -0300

    feat: init overview page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './pages/about/about.component';
+import { OverviewComponent } from './pages/overview/overview.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, OverviewComponent],
   template: `
     <app-header/>
     <app-home/>
     <app-about/>
+    <app-overview></app-overview>
     <router-outlet />
   `
 })
---
+++ src/app/pages/overview/overview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './overview.component.html'
+})
+export class OverviewComponent { }
