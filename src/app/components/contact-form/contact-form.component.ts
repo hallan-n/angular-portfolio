44ea48f6a07c260a6fb2d784f7f4bba6430359e2
hallan-n <hallanneves01@gmail.com>
2024-02-23 00:55:25 -0300

    feat: add handle submit forms

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,23 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, inject } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import { NgOptimizedImage } from '@angular/common';
-import {
-  FormControl,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
-
+import { Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule, MatSelectModule, MatInputModule, MatFormFieldModule, FormsModule, NgOptimizedImage],
+  imports: [
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    NgOptimizedImage,
+    MatButtonModule
+  ],
   templateUrl: './contact-form.component.html'
 })
 export class ContactFormComponent {
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  private snackBar = inject(MatSnackBar)
+  private formBuilderService = inject(FormBuilder)
+  protected form = this.formBuilderService.group({
+    name: ["", Validators.required],
+    email: ["", [
+      Validators.email, Validators.required
+    ]
+    ],
+    service: ["", Validators.required],
+    subject: ["", Validators.required],
+    message: ["", Validators.required],
+  })
+  isTouched: boolean = false
+  handleSubmitForm() {
+
+    if (this.form.valid) {
+      this.openSnackBar('Email enviado com sucesso!', 'Fechar')
+      this.form.reset()
+    }
+  }
+  
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action);
+  }
+
 }
