d6d80e7a6f8cc610daaf8020f4ae33bc9aac4875
hallan-n <hallanneves01@gmail.com>
2024-02-28 01:07:52 -0300

    Adjuste build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FLB4VJCF.mjs";import"./chunk-L76MO67Y.mjs";import"./chunk-OEQL325T.mjs";import"./chunk-4TIDA6GD.mjs";import"./chunk-RFZU3HHZ.mjs";import"./chunk-GEJJI7UY.mjs";import"./chunk-FQTFDFGH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-PEMVNAIG.mjs";import"./chunk-L76MO67Y.mjs";import"./chunk-OEQL325T.mjs";import"./chunk-4TIDA6GD.mjs";import"./chunk-RFZU3HHZ.mjs";import"./chunk-GEJJI7UY.mjs";import"./chunk-FQTFDFGH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
